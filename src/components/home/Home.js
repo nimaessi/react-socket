5c3cdae20a1fcfdeb2033705ab8a135a1eb01c3c
nimaessi <nimaesfahani92@gmail.com>
2023-06-13 13:46:58 +0330

    Add error component :wink:

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import MyNavBar from '../../module/NavBar';
 import DataTable from '../../module/DataTable';
-import { setData, fetchFail } from '../../features/network/networkSlice';
-import { useDispatch } from 'react-redux';
+import { setData, fetchFail, selectNetwork } from '../../features/network/networkSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import requestSocket from '../../api/socketReq';
+import Error from '../../module/Error';
 
 const Home = () => {
 
   const dispatch = useDispatch();
+  const network = useSelector(selectNetwork);
   useEffect(() =>{
     const ws = requestSocket();
       ws.onmessage = (event) => {
@@ -23,7 +25,7 @@ const Home = () => {
     <>
       <MyNavBar/>
       <Container className = 'd-flex align-items-center justify-content-center'>
-        <DataTable/>
+        {network.error ? <Error message = {'An error occurred'} /> : <DataTable />}
       </Container>
     </>
   )
---
+++ src/module/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Alert from 'react-bootstrap/Alert';
+import * as Icon from 'react-bootstrap-icons';
+const Error = ({message}) => {
+  return (
+    <Alert variant = 'danger' className = 'mt-5 w-50 text-center fw-bold'>
+        <Icon.ExclamationTriangle className = 'me-2' />
+        {message}
+    </Alert>
+    
+  )
+}
+
+export default Error;
